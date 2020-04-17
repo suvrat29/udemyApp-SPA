89d9d14d86222eaf8d150689837aa995afa783d5
Suvrat Mishra <suvrat29@gmail.com>
2020-04-17 23:11:11 +0530

    Finishing up registration process.

--- ClientApp/src/app/_services/auth.service.ts
+++ ClientApp/src/app/_services/auth.service.ts
@@ -19,6 +19,11 @@ export class AuthService {
             localStorage.setItem('token', user.token);
           }
         })
-      )
+      );
   }
+
+  register(model: any) {
+    return this.http.post(this.baseUrl + 'register', model);
+  }
+
 }
--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -10,24 +10,28 @@ import { HttpClient } from '@angular/common/http';
 export class HomeComponent implements OnInit {
   /** home ctor */
   registerMode = false;
-  values: any;
+  //values: any;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.getValues();
+    //this.getValues();
   }
 
   registerToggle() {
-    this.registerMode = !this.registerMode;
+    this.registerMode = true;
   }
 
-  getValues() {
-    this.http.get('http://localhost:5000/api/values').subscribe(response => {
-      this.values = response;
-    }, error => {
-      console.log(error);
-    });
+  //getValues() {
+  //  this.http.get('http://localhost:5000/api/values').subscribe(response => {
+  //    this.values = response;
+  //  }, error => {
+  //    console.log(error);
+  //  });
+  //}
+
+  cancelRegisterMode(registerMode: boolean) {
+    this.registerMode = registerMode;
   }
 
 }
